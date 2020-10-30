44ffa9728e2ce78893c4433455f525af8a711c3e
Ruan Arcega <ruanarcega@gmail.com>
2020-10-30 19:29:40 -0300

    add opts

--- src/app/jobs/UserReport.js
+++ src/app/jobs/UserReport.js
@@ -1,5 +1,8 @@
 export default {
     key: 'UserReport',
+    options: {
+        delay: 5000,
+    },
     async handle({ data }) {
         const { user } = data;
 
--- src/app/lib/Queue.js
+++ src/app/lib/Queue.js
@@ -8,13 +8,14 @@ const queues = Object.values(jobs).map(job => ({
     bull: new Queue(job.key, redisConfig),
     name: job.key,
     handle: job.handle,
+    options: job.options,
 }));
 
 export default {
     queues,
     add(name, data) {
         const queue = this.queues.find(queue => queue.name == name);
-        return queue.bull.add(data);
+        return queue.bull.add(data. queue.options);
     },
     process() {
         return this.queues.forEach(queue => {
